c1941766a78c5156dbf5df7fc1896ffb117b3ec3
Dzianis Narubin <Dzianis.Narubin@thomsonreuters.com>
2021-01-26 20:58:05 +0300

    Final Verstion

--- ICanWin/ICanWin/PageObjects/PastebinResultsPage.cs
+++ ICanWin/ICanWin/PageObjects/PastebinResultsPage.cs
@@ -13,13 +13,14 @@ namespace ICanWin
         
         public PastebinResultsPage(string url)
         {
+            //Open Result Page
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("no-sandbox");
             driverRP = new ChromeDriver(ChromeDriverService.CreateDefaultService(), options, TimeSpan.FromMinutes(3));
             driverRP.Navigate().GoToUrl(url);
             driverRP.Manage().Timeouts().PageLoad.Add(System.TimeSpan.FromSeconds(30));
         }       
-        
+        //Get Pasted Value
         public string GetPasteValue()
         {
             WebDriverWait wait = new WebDriverWait(driverRP, TimeSpan.FromSeconds(10));
@@ -27,21 +28,6 @@ namespace ICanWin
             string paste = pasteResult.GetAttribute("value");
             return paste;
         }
-        public string GetTitleValue()
-        {
-            WebDriverWait wait = new WebDriverWait(driverRP, TimeSpan.FromSeconds(10));
-            IWebElement titleResult = wait.Until(e => e.FindElement(By.XPath("//div[@class='info-top']/h1")));
-            string title = titleResult.GetAttribute("value");
-            return title;
-
-        }
-        public string GetExpitationTime()
-        {
-            WebDriverWait wait = new WebDriverWait(driverRP, TimeSpan.FromSeconds(10));
-            IWebElement titleResult = wait.Until(e => e.FindElement(By.XPath("//div[@class='expire']")));
-            string expTimeRes = titleResult.GetAttribute("value");
-            return expTimeRes;
-        }
-
+ 
     }
 }
