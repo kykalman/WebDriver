ee88ce78de47f2b476e91b873bf83c88f0eaac16
Dziais Narubin <Dziais_Narubin@epam.com>
2021-01-30 02:36:00 +0300

    Added Assert for HurtMePlenty

--- HurtMePlenty/HurtMePlenty/PageObjects/CalculationResults.cs
+++ HurtMePlenty/HurtMePlenty/PageObjects/CalculationResults.cs
@@ -12,30 +12,81 @@ namespace HurtMePlenty.PageObjects
     {
         public static IWebDriver dricerRP;
 
-        public CalculationResults(GoogleCloudHomePage obj)
+        public CalculationResults()
         {
             dricerRP = driver;
         }
         public string GetVMClassValue()
         {
-            //VM Class, Instance type, Region, local SSD, commitment term
-            return ;
+
+            //Get VM Class Value
+            IWebElement vmClass = GetElementByXpath("//*[@id='compute']/md-list/md-list-item[2]/div");
+            string vmClassValue = vmClass.GetAttribute("innerHTML");            
+            
+            int index = vmClassValue.IndexOf(':');
+            vmClassValue = vmClassValue.Substring(index + 2);
+            int indexOfSumbol = vmClassValue.IndexOf('\r');
+            vmClassValue = vmClassValue.Substring(0, vmClassValue.Length - (vmClassValue.Length - indexOfSumbol));
+            return vmClassValue;
         }
         public string GetInstanceType()
         {
+            //Get Instance Type Value
+            IWebElement instType = GetElementByXpath("//*[@id='compute']/md-list/md-list-item[3]/div");
+            string instTypeValue = instType.GetAttribute("innerHTML");
+            int index = instTypeValue.IndexOf(':');
+            instTypeValue = instTypeValue.Substring(index + 2);
+            int indexOfSumbol = instTypeValue.IndexOf('\r');
+            instTypeValue = instTypeValue.Substring(0, instTypeValue.Length - (instTypeValue.Length - indexOfSumbol));
+            return instTypeValue;
 
         }
         public string GetRegion()
         {
-
+            //Get Region Value
+            IWebElement region = GetElementByXpath("//*[@id='compute']/md-list/md-list-item[4]/div");
+            string regionValue = region.GetAttribute("innerHTML");
+            int index = regionValue.IndexOf(':');
+            regionValue = regionValue.Substring(index+2);
+            int indexOfSumbol = regionValue.IndexOf('\r');
+            regionValue = regionValue.Substring(0, regionValue.Length - (regionValue.Length - indexOfSumbol));
+            return regionValue;            
         }
         public string GetLocalSSD()
         {
-
+            //Get Local SSD Value
+            IWebElement localSSD = GetElementByXpath("//*[@id='compute']/md-list/md-list-item[8]/div");
+            string localSSDValue = localSSD.GetAttribute("innerHTML");
+            int index = localSSDValue.IndexOf('2');
+            localSSDValue = localSSDValue.Substring(index);
+            int indexOfSumbol = localSSDValue.IndexOf('5');
+            localSSDValue = localSSDValue.Substring(0, localSSDValue.Length - (localSSDValue.Length - (indexOfSumbol+1)));
+            return localSSDValue;
         }
         public string GetCommitmentTerm()
         {
+            //Get Cimmitment term
+            IWebElement commitTerm = GetElementByXpath("//*[@id='compute']/md-list/md-list-item[9]");
+            string commitTermValue = commitTerm.GetAttribute("innerHTML");
+            int index = commitTermValue.IndexOf('1');
+            commitTermValue = commitTermValue.Substring(index);
+            int indexOfSumbol = commitTermValue.IndexOf('\r');
+            commitTermValue = commitTermValue.Substring(0, commitTermValue.Length - (commitTermValue.Length - (indexOfSumbol)));
+            return commitTermValue;
+        }
+        public string GetCalculatedPrice()
+        {
+            //Get Expected Price
+            string sumStartPage;
+            IWebElement sumStartElement = GetElementByXpath("//*[@id='compute']/md-list/md-list-item[10]/div/b");
+            sumStartPage = sumStartElement.GetAttribute("innerHTML");
 
+            sumStartPage.Replace(" ", "");
+            int indexOfU = sumStartPage.IndexOf('U');
+            sumStartPage = sumStartPage.Substring(indexOfU);
+            int indexOfSumbol = sumStartPage.IndexOf('\r');
+            sumStartPage = sumStartPage.Substring(0, sumStartPage.Length - (sumStartPage.Length - indexOfSumbol));
+            return sumStartPage;
         }
     }
 }
--- HurtMePlenty/HurtMePlenty/Tests/CalculateCluodResources.cs
+++ HurtMePlenty/HurtMePlenty/Tests/CalculateCluodResources.cs
@@ -19,8 +19,7 @@ namespace HurtMePlenty
         [TestMethod]
         public void CalculateComputeEngine()
         {
-            string siteURL = "https://cloud.google.com";
-            string mailBoxURL = "https://10minutemail.com";
+            string siteURL = "https://cloud.google.com";            
             string searchText = "Google Cloud Platform Pricing Calculator";
             string selectComponent="Compute Engine";
             int numberOfInstances = 4;
@@ -43,8 +42,40 @@ namespace HurtMePlenty
             homePage.SelectCommitedUsage();
             Thread.Sleep(10000);
             homePage.AddToEstimate();
-            CalculationResults results = new CalculationResults(homePage);
+
+            CalculationResults results = new CalculationResults();
+            string vmClassExp = "regular";
+            string instTypeExp = "n1-standard-8";
+            string regoinExp = "Oregon";
+            string ssdExp = "2x375";
+            string commitTermExp = "1 Year";
+            string priceExp = "USD 5,413.06";
+
+
+            string vmClassRes = results.GetVMClassValue();
+            string instTypeRes = results.GetInstanceType();
+            string regoinRes = results.GetRegion();
+            string ssdRes = results.GetLocalSSD();
+            string commitTermRes = results.GetCommitmentTerm();
+            string priceRes = results.GetCalculatedPrice();
+
+
+            Assert.AreEqual(vmClassExp, vmClassRes);
+            Assert.AreEqual(instTypeExp, instTypeRes);
+            Assert.AreEqual(regoinExp, regoinRes);
+            Assert.AreEqual(ssdExp, ssdRes);
+            Assert.AreEqual(commitTermExp, commitTermRes);
+            Assert.AreEqual(priceExp, priceRes);
+
+            Thread.Sleep(10000);
             
+        }
+        [TestCleanup]
+        public void CloseBrowser()
+        {
+            //Close Result Page Browser
+            CalculationResults.dricerRP.Quit();
+            CalculationResults.dricerRP = null;
         }
     }
 }
